b68f8738eefb7bf4e33b113b1c36dd59f5114418
Kara Crumpton <magicalmonkies@gmail.com>
2025-04-22 23:02:14 -0400

    Adding stuff...

---
+++ Slot Machine/MiniGame.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace KCrumpton_CPT_206_Final_Project_Slot_Machine
+{
+    partial class MiniGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MiniGame));
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            this.picBox2 = new System.Windows.Forms.PictureBox();
+            this.picBox3 = new System.Windows.Forms.PictureBox();
+            this.picBox4 = new System.Windows.Forms.PictureBox();
+            this.picBox5 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBox1
+            // 
+            this.picBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBox1.BackgroundImage")));
+            this.picBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picBox1.Location = new System.Drawing.Point(159, 362);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(153, 166);
+            this.picBox1.TabIndex = 0;
+            this.picBox1.TabStop = false;
+            this.picBox1.Click += new System.EventHandler(this.picBox1_Click);
+            // 
+            // picBox2
+            // 
+            this.picBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBox2.BackgroundImage")));
+            this.picBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picBox2.Location = new System.Drawing.Point(765, 321);
+            this.picBox2.Name = "picBox2";
+            this.picBox2.Size = new System.Drawing.Size(153, 166);
+            this.picBox2.TabIndex = 1;
+            this.picBox2.TabStop = false;
+            this.picBox2.Click += new System.EventHandler(this.picBox2_Click);
+            // 
+            // picBox3
+            // 
+            this.picBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBox3.BackgroundImage")));
+            this.picBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picBox3.Location = new System.Drawing.Point(956, 378);
+            this.picBox3.Name = "picBox3";
+            this.picBox3.Size = new System.Drawing.Size(153, 166);
+            this.picBox3.TabIndex = 2;
+            this.picBox3.TabStop = false;
+            this.picBox3.Click += new System.EventHandler(this.picBox3_Click);
+            // 
+            // picBox4
+            // 
+            this.picBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBox4.BackgroundImage")));
+            this.picBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picBox4.Location = new System.Drawing.Point(372, 311);
+            this.picBox4.Name = "picBox4";
+            this.picBox4.Size = new System.Drawing.Size(153, 166);
+            this.picBox4.TabIndex = 3;
+            this.picBox4.TabStop = false;
+            this.picBox4.Click += new System.EventHandler(this.picBox4_Click);
+            // 
+            // picBox5
+            // 
+            this.picBox5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBox5.BackgroundImage")));
+            this.picBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picBox5.Location = new System.Drawing.Point(570, 420);
+            this.picBox5.Name = "picBox5";
+            this.picBox5.Size = new System.Drawing.Size(153, 166);
+            this.picBox5.TabIndex = 4;
+            this.picBox5.TabStop = false;
+            this.picBox5.Click += new System.EventHandler(this.picBox5_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Viner Hand ITC", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(180, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(900, 249);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "They say the counselors are still here... Eyes wide. \r\nMouths full of lakewater.." +
+    ". \r\nChoose a photo... maybe you can join them...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MiniGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1233, 675);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picBox5);
+            this.Controls.Add(this.picBox4);
+            this.Controls.Add(this.picBox3);
+            this.Controls.Add(this.picBox2);
+            this.Controls.Add(this.picBox1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "MiniGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MiniGame";
+            this.Load += new System.EventHandler(this.MiniGame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox1;
+        private System.Windows.Forms.PictureBox picBox2;
+        private System.Windows.Forms.PictureBox picBox3;
+        private System.Windows.Forms.PictureBox picBox4;
+        private System.Windows.Forms.PictureBox picBox5;
+        private System.Windows.Forms.Label label1;
+    }
+}
